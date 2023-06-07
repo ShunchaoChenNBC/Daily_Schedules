8b7e91b9de2f5fd891c5d4b701ba7218f4b065ee
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-06-06 21:17:54 -0400

    Create VOD_AD_LOAD_Dash_Table.sql

---
+++ VOD_AD_LOAD_Dash_Table.sql
@@ -0,0 +1,18 @@
+
+select 
+adobe_date,
+Account_Entitlement,
+extract(week from adobe_date) as Week,
+set_duration,
+Devices,
+trim(Primary_Genre) as Primary_Genre, -- remove whitespace
+Content_Types,
+Ad_Pod_Name,
+sum(num_views_started) as Content_Start,
+round(sum(ad_viewed),0) as Ad_Unit,
+round(sum(Ad_Time_Watched)/60,0) as Ad_Minutes_Watched,
+round(sum(num_seconds_played_no_ads)/3600,2) as Hours_Watched
+from 
+`nbcu-ds-sandbox-a-001.Shunchao_Sandbox.VOD_AD_LOAD_Table` 
+where adobe_date = current_date("America/New_York")-1
+group by 1,2,3,4,5,6,7,8
